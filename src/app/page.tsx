3a43839e041d50170631bacf752d050efb852aa7
Samuel Davis <me@notbruce.dev>
2024-03-03 12:33:56 +0000

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,44 +15,9 @@ export default function Home() {
       <div className={styles['me']}>
         <Image src={Profile} alt="Avatar" />
         <div className={styles['me-right']}>
-            <span className={styles['name']}>Hello, I&apos;m Samuel Davis</span>
+            <span className="text-3xl font-bold underline">Hello, I&apos;m Samuel Davis</span>
           <span className={styles['description']}>I&apos;m a Software Engineer & Web Developer.</span>
         </div>
-        <div className={styles['socials']}>
-          <a href='https://github.com/NotBruce' className={styles['social']}>
-            <FaGithub className={styles['social-icon']} />
-            <span className={'social-name'}>GitHub</span>
-          </a>
-          <a href='https://discord.com/users/771891131499675668' className={styles['social']}>
-            <FaDiscord className={styles['social-icon']} />
-            <span className={'social-name'}>Discord</span>
-          </a>
-          <a href='mailto:me@notbruce.dev' className={styles['social']}>
-            <MdEmail className={styles['social-icon']} />
-            <span className={'social-name'}>Email</span>
-          </a>
-        </div>
-      </div>
-      <span className={styles['projects-title']}>Projects</span>
-      <div className={styles['projects']}>
-        <Project
-          title="RaidHub"
-          description="A fast website that provides raid statistics like completions, lowmans and much more to come"
-          image={RaidHub}
-          link='https://raidhub.io/'
-        />
-        <Project
-          title="Loti"
-          description="Versatile Discord Bot For Moderation, Social Commands, and More!"
-          image={Loti}
-          link='https://www.loti.dev/'
-        />
-        <Project
-          title="Felony"
-          description="Versatile Discord Bot For Moderation, Social Commands, and More!"
-          image={Felony}
-          link='https://felony.me/'
-        />
       </div>
     </div>
   );
